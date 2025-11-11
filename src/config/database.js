747b2b9eb0e1da6c96bdca6b11a6a6ce0a9ce901
Asus <faris.m.saputra@gmail.com>
2025-11-11 17:19:15 +0700

    pengelolaan data album

---
+++ src/config/database.js
@@ -0,0 +1,44 @@
+const { Pool } = require('pg');
+
+class Database {
+  constructor() {
+    this._pool = null;
+  }
+
+  init() {
+    this._pool = new Pool({
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+    });
+
+    this._pool.on('error', (err) => {
+      console.log('Unexpected error on idle client', err);
+      process.exit(1);
+    });
+  }
+
+  get pool() {
+    if (!this._pool) {
+      this.init();
+    }
+    return this._pool;
+  }
+
+  async query(text, params) {
+    const start = Date.now();
+    try {
+      const res = await this.pool.query(text, params);
+      const duration = Date.now() - start;
+      console.log('Executed query', { text, duration, rows: res.rowCount });
+      return res;
+    } catch (error) {
+      console.log('Error executing query', { text, error });
+      throw error;
+    }
+  }
+}
+
+module.exports = Database;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,27 @@
-const routes = (handler) => [
+const albumsHandler = require('../handler/albumsHandler');
+const songsHandler = require('../handler/songsHandler');
+
+const routes = [
+  // ALBUMS ROUTES
   {
     method: 'POST',
     path: '/albums',
-    handler: handler.postAlbumHandler,
+    handler: albumsHandler.postAlbumHandler,
   },
   {
     method: 'GET',
     path: '/albums/{id}',
-    handler: handler.getAlbumByIdHandler,
+    handler: albumsHandler.getAlbumByIdHandler,
   },
   {
     method: 'PUT',
     path: '/albums/{id}',
-    handler: handler.putAlbumByIdHandler,
+    handler: albumsHandler.putAlbumByIdHandler,
   },
   {
     method: 'DELETE',
     path: '/albums/{id}',
-    handler: handler.deleteAlbumByIdHandler,
+    handler: albumsHandler.deleteAlbumByIdHandler,
   },
 ];
 
